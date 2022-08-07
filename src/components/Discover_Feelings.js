5f970a6e99dcfca5d0869dcd6713704509633e01
yoons <kys5037k@naver.com>
2022-08-08 04:40:02 +0900

    [yunseong]페르소나 카드 기본 틀

--- src/components/Discover_Feelings.js
+++ src/components/Discover_Feelings.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-function Discover_feelings({ id, user_id, image, name, intro, type }) {
+function Discover_feelings({ id, user_id, image, name, intro, persona, type, card_persona }) {
     const [hover, setHover] = useState(false)
     const feelings_style = {
         backgroundImage: `url(${image})`,
@@ -16,17 +16,27 @@ function Discover_feelings({ id, user_id, image, name, intro, type }) {
             </div>
             <div className="user_data">
                 <p id="name">{name}</p>
-                <p id="intro">{intro}</p>
+                <p id="intro">{intro}</p> 
+                <button id="btn_profile">프로필 보기</button>
+                <button id="btn_following">팔로잉</button>
+                <div id="persona">{persona}</div>
                 <p id="type">{type}</p>
+                <div id="card_persona">{card_persona}</div>
+                <div id="card_persona_2">{card_persona}</div>
             </div>
         </div>
     )
 
-
 } export default Discover_feelings;
 
 
-
+// function render_persona({id, persona}) {
+//     return (
+//         <div className="card_persona" id={"persona" + id}>
+//             <p id="user_persona">{persona}</p>
+//         </div>
+//     )
+// }
 
 
 function render_Discover_feelings(fill) {
@@ -34,8 +44,6 @@ function render_Discover_feelings(fill) {
         <Discover_feelings key={fill.id}
             user_id={fill.user_id} index={fill.index}
             image={fill.image} persona={fill.persona}
-            title={fill.title} body={fill.body}
-            date_time={fill.date_time} />
+            title={fill.title} card_persona={fill.body} />
     )
 }
-
