f86f7f5848c45b035a068800145fa5df7c9df063
AsherOh <wnstj701@naver.com>
2022-08-08 23:25:18 +0900

    로그인 UI 구현

--- src/routes/Mainpage.js
+++ src/routes/Mainpage.js
@@ -2,12 +2,88 @@ import Header from "../components/Header";
 import { Footer2 } from "../components/Footer";
 import styles from "../static/css/style.module.css";
 import Navbar from "../components/Navbar";
+import TextField from '@mui/material/TextField';
+import Checkbox from '@mui/material/Checkbox';
+import Button from '@mui/material/Button';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Avatar from '@mui/material/Avatar';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+
 function Mainpage() {
+
     return (
         <>
             <Header />
             <Navbar />
-            <div className={styles.wrap}></div>
+            <div className={styles.wrap}>
+                <Container component="main" maxWidth="sm">
+                    <Box
+                        sx={{
+                            marginTop: 25,
+                            display: 'flex',
+                            flexDirection: 'column',
+                            alignItems: 'center',
+                        }}
+                    >
+                        {/* <Avatar sx={{ m: 1, bgcolor: 'success.light' }}>
+                            <LockOutlinedIcon />
+                        </Avatar> */}
+                        <Typography
+                            component="h1"
+                            variant="h5"
+                            sx={{ mb: 5 }}
+                        >
+                            로그인하여 친구들의 멀티 페르소나를 만나보세요
+                        </Typography>
+                        <TextField
+                            label="Email Address"
+                            name="email"
+                            required
+                            fullWidth
+                            autoComplete="email"
+                            autoFocus
+                            sx={{ mb: 3 }}
+                        />
+                        <TextField
+                            label="password"
+                            name="password"
+                            type="password"
+                            required
+                            fullWidth
+                            autoComplete="current-password"
+                        />
+                        <FormControlLabel
+                            control={<Checkbox value="remember" color="primary" />}
+                            label="Remember me"
+                        />
+                        <Button
+                            type="submit"
+                            variant="contained"
+                            fullWidth
+                            sx={{ mt: 3, mb: 2, bgcolor: '#3CDA9F' }}
+                        >로그인하기</Button>
+                        <Grid container>
+                            <Grid item xs>
+                                <Link href="#" variant="body2">
+                                    비밀번호 찾기
+                                </Link>
+                            </Grid>
+                            <Grid item>
+                                <Link href="#" variant="body2">
+                                    회원가입
+                                </Link>
+                            </Grid>
+                        </Grid>
+                    </Box>
+                </Container>
+            </div>
             <Footer2 />
         </>
     );
