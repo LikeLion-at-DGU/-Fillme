95eb4eaf6eba09cf7ddad57c121c12c3b59ccb2c
yoons <kys5037k@naver.com>
2022-08-10 05:02:02 +0900

    Modal

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,20 @@
 import "../static/css/Header.css";
+import styles from "../static/css/style.module.css";
 import { NavLink } from "react-router-dom";
 function Header() {
     return (
         <>
             <div className="header">
                 <NavLink to="/Feed">
-                    <section id="header_logo">Fill Me</section>
+                    <button id="header_logo">Fill Me</button>
+                </NavLink>
+                <NavLink to="/Modal">
+                    <img
+                        className={styles.icon}
+                        id="bell"
+                        src="images/bell.png"
+                        alt="New"
+                    />
                 </NavLink>
             </div>
         </>
