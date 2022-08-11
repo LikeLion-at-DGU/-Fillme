f4b9ed51d8a2b50b011c2f917eb5edb78f23dff8
yoons <kys5037k@naver.com>
2022-08-11 23:57:42 +0900

    Modal

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,43 @@
 import "../static/css/Header.css";
 import styles from "../static/css/style.module.css";
 import { NavLink } from "react-router-dom";
+import React, {useState} from "react";
+import Modal from "react-modal";
+import ModalContents from "react-modal";
+
+Modal.setAppElement('#root')
+
 function Header() {
+    const [modalOpen, setModalOpen] = useState(false);
+
     return (
         <>
-            <div className="header">
+            <div className="header" style={{position: "relative"}}>
                 <NavLink to="/Feed">
                     <section id="header_logo">Fill Me</section>
                 </NavLink>
                 <NavLink to="/Feed">
-                    <img
-                        className={styles.icon}
-                        id="bell"
-                        src="images/bell.png"
-                        alt="New"
-                    />
+                    
                 </NavLink>
+
+                <div className="modal">
+                    <button type="button" onClick={() => setModalOpen(!modalOpen)}>
+                        <img 
+                        className={styles.icon} 
+                        id="bell" src="images/bell.png" 
+                        alt="New"/>
+                    </button>
+                    <div>
+                        <Modal isOpen={modalOpen}
+                            className="customModal">
+                            <div className="News">
+                                <p id="Today">오늘</p>
+                                <p id="ThisWeek">이번 주</p>
+                                <p id="ThisMonth">이번 달</p>
+                            </div>
+                        </Modal>
+                    </div>
+                </div>
             </div>
         </>
     );
