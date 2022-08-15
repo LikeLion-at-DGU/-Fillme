83a5a133f0487200713b70375bcfb2064e7ed06d
yoons <kys5037k@naver.com>
2022-08-15 19:11:08 +0900

    0815/19:11

---
+++ src/components/Detail_image.js
@@ -0,0 +1,27 @@
+import dummy from "../data.json";
+import "../static/css/Detail_image.css";
+
+function DetailImage({img}) {
+
+    const BigImage = {
+        backgroundImage: `url(${img})`,
+        backgroundSize: "cover",
+        backgroundRepeat: `no-repeat`,
+        backgroundPosition: `center`,
+    };
+
+    // const SmallImage = {
+    //     backgroundImage: `url(${imageUrl})`,
+    //     backgroundSize: "cover",
+    //     backgroundRepeat: `no-repeat`,
+    //     backgroundPosition: `center`,
+    // };
+
+    return (
+        <div className="Photo">
+            <div className="Big_image" style={BigImage}>
+            </div>
+        </div>
+    )
+
+} export default DetailImage;
