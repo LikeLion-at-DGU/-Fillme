e88477d195312922ce5b2ca13be27f388de050d1
yaena1223 <yaena1223@dgu.ac.kr>
2022-08-17 04:45:59 +0900

    다른사용자 조회 페이지, 검색하면 들어갈 수 있도록

--- src/components/Persona_Card.js
+++ src/components/Persona_Card.js
@@ -3,13 +3,14 @@ import { useEffect } from "react";
 import axios from "axios";
 import { useState } from "react";
 import Logout from "../routes/Logout";
-function Persona_Card({ username, fullname, memo, color, image }) {
+function Persona_Card({ user, fullname, memo, color, image }) {
+    const loginusername = localStorage.getItem("loginUserName");
     const imageUrl = "http://127.0.0.1:8000" + image;
 
     return (
         <>
             <div className={styles.card} style={{ backgroundColor: color }}>
-                <section className={styles.user_id}>@{username}</section>
+                <section className={styles.user_id}>@{loginusername}</section>
                 <section className={styles.user_name}>{fullname}</section>
                 <section className={styles.user_memo}>{memo}</section>
                 <img src={imageUrl} className={styles.image_box} />
