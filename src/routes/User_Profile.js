86e40414a21892954249415d7d6f938b43635794
AsherOh <wnstj701@naver.com>
2022-08-17 15:06:02 +0900

    팔로우 팔로잉 성공

--- src/routes/User_Profile.js
+++ src/routes/User_Profile.js
@@ -4,16 +4,18 @@ import styles from "../static/css/style.module.css";
 import Navbar from "../components/Navbar";
 import Persona_Card from "../components/Persona_Card";
 import Logout from "./Logout";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import axios from "axios";
 import My_persona_card from "../components/My_persona_card";
 import { Link, useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import { useParams } from "react-router-dom";
 import User_persona_card from "../components/User_persona_card";
+
 function User_Profile() {
     const { user_id } = useParams();
     let request = JSON.parse(localStorage.getItem("user_profile_data"));
+    const Id = request.id;
 
     useEffect(() => {
         request = JSON.parse(localStorage.getItem("user_profile_data"));
@@ -28,6 +30,33 @@ function User_Profile() {
             />
         )),
     ];
+
+    let followState = useRef("팔로우");
+    const onChange = async (e) => {
+        e.preventDefault();
+        if (followState.current === "팔로우") {
+            followState.current = "팔로잉";
+            console.log("팔로우 버튼 눌렀을 때", followState.current);
+            await axios.post(`http://127.0.0.1:8000/mypage/follow/${Id}/`)
+                .then((res) => {
+                    console.log(res, "팔로우 성공");
+                })
+                .catch((res) => {
+                    console.log(res, "팔로우 실패");
+                })
+        } else {
+            followState.current = "팔로우";
+            console.log("팔로우 취소 눌렀을 때", followState.current);
+            await axios.post(`http://127.0.0.1:8000/mypage/follow/${Id}/`)
+                .then((res) => {
+                    console.log(res, "팔로우 취소 성공");
+                })
+                .catch((res) => {
+                    console.log(res, "팔로우 취소 실패");
+                })
+        };
+    };
+
     return (
         <>
             <Navbar />
@@ -43,6 +72,12 @@ function User_Profile() {
                         image={request.image}
                     />
                 </div>
+                <button
+                    className={followState.current === "팔로우" ? styles.followBtn : styles.beforeFollowBtn}
+                    onClick={onChange}
+                >
+                    {followState.current}
+                </button>
                 <div className={styles.persona_card}>{[personaCard]}</div>
             </div>
             <Footer />
