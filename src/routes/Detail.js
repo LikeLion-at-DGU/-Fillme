fcf4dcbc55db42a10368b9967993fe356909364d
yoons02 <kys5037k@naver.com>
2022-08-18 01:30:18 +0900

    detail_image

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -2,16 +2,23 @@ import Navbar from "../components/Navbar"
 import { Footer } from "../components/Footer";
 import dummy from "../data.json"
 import DetailImage from "../components/Detail_image"
+import styles from "../static/css/style.module.css";
 // import DetailImage from "../components/Detail_image"
 
 function Detail() {
 
     return(
     <>
-    <h3>dks</h3>
+    <style>
+                {`#discover {
+                    filter: invert(67%) sepia(37%) saturate(660%) hue-rotate(106deg)
+                    brightness(120%) contrast(95%);}`}
+            </style>
+            <div className={styles.wrap}>
     <DetailImage
         img={dummy.image}
         />
+        </div>
     <Footer />
     </>
     );
