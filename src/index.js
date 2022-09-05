1201ce3944c4581e9119c0f6c0c5c495710d33c7
yaena1223 <yaena1223@dgu.ac.kr>
2022-09-05 23:34:25 +0900

    0905 페이지 넘어갈때 스크롤 젤 위로

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import ScrollToTop from "./components/ScrollToTop";
-import { BrowserRouter } from "react-router-dom";
+
 <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"></meta>;
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
